f179f072a88f0807e48c1f72f0627d88ecb81d42
elberth <eagreda@lucasian.com>
2018-09-10 06:45:58 -0500

    add components

--- triviApp/src/app/app.module.ts
+++ triviApp/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { Nivel1Component } from './home/nivel1/nivel1.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    Nivel1Component
   ],
   imports: [
     BrowserModule
---
+++ triviApp/src/app/shared/config/app.variables.ts
@@ -0,0 +1,5 @@
+export var AppVariable = {
+    baseurl: 'http://192.168.10.9:7001/eleccionesp.ws-1.0/rest',
+    grupos: 'www',
+    preguntas: 'preg'
+};
