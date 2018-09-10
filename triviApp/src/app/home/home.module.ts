63e061e3dcb0479a2d733e502232719fbc872849
elberth <eagreda@lucasian.com>
2018-09-10 15:17:03 -0500

    validate question and answer

--- triviApp/src/app/home/home.module.ts
+++ triviApp/src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }             from '@angular/core';
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 // Routing
 import { HomeRouting } from './home.routing';
 // Components
@@ -11,7 +12,8 @@ import { CustomSevice } from '../shared/services/custom.service';
 @NgModule({
     imports: [
         CommonModule,
-        HomeRouting
+        HomeRouting,
+        FormsModule
     ],
     declarations: [
         HomeComponent,
--- triviApp/src/app/home/nivel1/nivel1.component.ts
+++ triviApp/src/app/home/nivel1/nivel1.component.ts
@@ -10,25 +10,65 @@ export class Nivel1Component implements OnInit {
   @Input() preguntas:any;
   private preguntaActiva:any;
   private numQuestion:number;
+  private optionAnswer:string;
+  private showQuestion:boolean;
+  private showIntroduction:boolean;
+  private countAnswer:number;
+  private countQuestion:number;
 
   constructor() {
     this.numQuestion = 0;
+    this.showIntroduction = true;
+    this.showQuestion = false;
+    this.countAnswer = 0;
   }
 
   ngOnInit() {
     this.preguntaActiva = this.preguntas[this.numQuestion];
-    console.log(this.preguntaActiva);
+    this.countQuestion = this.preguntas.length;
+  }
+
+  private showQuestions():void{
+    this.showIntroduction = false;
+    this.showQuestion = true;
   }
 
   private nextQuestion():void{
     // Validar si la respuesta es correcta
-    alert("respuesta correcta");
-
+    if(this.preguntaActiva.post_meta_fields.respuesta_correcta == this.optionAnswer){
+      this.countAnswer = this.countAnswer + 1;
+      alert("Respuesta Correcta");
+    }
+    else{
+      alert("Respuesta Incorrecta");
+    }
     // Pasar a la siguiente pregunta
     this.numQuestion = this.numQuestion + 1;
-    console.log(this.numQuestion);
-    this.preguntaActiva = this.preguntas[this.numQuestion];
-    console.log(this.preguntaActiva);
+    if(this.preguntas[this.numQuestion] == undefined || this.preguntas[this.numQuestion] == null){
+      this.finishLevel();
+    }else{
+      this.preguntaActiva = this.preguntas[this.numQuestion];
+      this.showIntoductions();
+    }
+  }
+
+  private finishLevel():void{
+    this.showQuestion = false;
+    this.preguntaActiva = [];
+    //var tmpResult = Math.round(this.countAnswer / this.countQuestion);
+    //console.log(tmpResult);
+    if(this.countAnswer >= 2){
+      alert("Gano el nivel");
+    }
+    else{
+      alert("Perdiste tonto");
+    }
+    alert("numero de respuestas correctas: "+this.countAnswer + " de "+this.countQuestion);
+  }
+
+  private showIntoductions():void{
+    this.showIntroduction = true;
+    this.showQuestion = false;
   }
 
 }
