20ad3b7f3794622f2f9147d0d3ca4e4771e200c1
elberthAgreda <elberth.agreda@gmail.com>
2018-09-10 20:25:31 -0500

    Cambio de url services a protocolo https

--- triviApp/src/app/shared/config/app.variables.ts
+++ triviApp/src/app/shared/config/app.variables.ts
@@ -1,5 +1,5 @@
 export var AppVariable = {
-    baseurl: 'http://unoraya.com/demo/triviaApp/wp-json/wp/v2/',
+    baseurl: 'https://unoraya.com/demo/triviaApp/wp-json/wp/v2/',
     grupos: 'grupos/',
     preguntas: 'preguntas/'
 };
