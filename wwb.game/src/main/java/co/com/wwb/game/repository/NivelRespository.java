06d4612b8e9f1ee7daf40f1711f6423a7272680c
Unknown <elberth.agreda@gmail.com>
2018-09-28 20:44:59 -0500

    se elimina carpeta duplicada

--- wwb.game/src/main/java/co/com/wwb/game/repository/NivelRespository.java
+++
@@ -1,9 +0,0 @@
-package co.com.wwb.game.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import co.com.wwb.game.db.Nivel;
-
-public interface NivelRespository extends CrudRepository<Nivel, String>{
-
-}
--- wwb.game/src/main/java/co/com/wwb/game/repository/UsuarioRespository.java
+++
@@ -1,9 +0,0 @@
-package co.com.wwb.game.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import co.com.wwb.game.db.Usuario;
-
-public interface UsuarioRespository extends CrudRepository<Usuario, String>{
-
-}
