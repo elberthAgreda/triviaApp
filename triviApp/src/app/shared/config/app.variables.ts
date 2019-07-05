35e375f92663ffdcabd39d3441855ddc2c453599
Elberth agreda <eagreda@lucasian.com>
2019-07-05 09:49:44 -0500

    ajustes front

--- triviApp/src/app/shared/config/app.variables.ts
+++ triviApp/src/app/shared/config/app.variables.ts
@@ -1,7 +1,6 @@
 export const AppVariable = {
     baseurl: 'https://www.cincobmm.com/unoraya/wp-json/wp/v2/',
-    //baseGame: 'https://www.cincobmm.com/rest/wwbgame/api/',
-    baseGame: 'http://triviaservices-env.mtm9ueucne.us-east-2.elasticbeanstalk.com/rest/wwbgame/api/',
+    baseGame: 'https://triviaservices-env.mtm9ueucne.us-east-2.elasticbeanstalk.com/rest/wwbgame/api/',
     saveProgress: 'saveProgress',
     gurdarVoto: 'saveVote',
     puntos: 'scores',
