829041d2c75c9b98cd3320822387ea7cd38450f5
luisito666 <luispenagos91@gmail.com>
2021-08-21 11:06:13 -0500

    Enable config for cors and add Dockerfiles and compose for testing

--- backend/wwb.game/src/main/java/co/com/wwb/game/Application.java
+++ backend/wwb.game/src/main/java/co/com/wwb/game/Application.java
@@ -1,7 +1,10 @@
 package co.com.wwb.game;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -11,13 +14,13 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 	
-	 /*@Bean
-	 public WebMvcConfigurer corsConfigurer() {
-		 return new WebMvcConfigurerAdapter() {
-			 @Override
-			 public void addCorsMappings(CorsRegistry registry) {
-				 registry.addMapping("/**").al;
-			 }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
 		 };
-	 }*/
+	 }
 }
