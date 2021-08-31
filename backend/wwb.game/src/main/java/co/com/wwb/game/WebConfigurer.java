e5e507c0f7a0e5bf770a07d09616f05ac578232d
Carlos Zuluaga <carlos.zuluaga@gmail.com>
2021-08-30 21:14:37 -0500

    Cambios para habilitar cabeceras para cors

--- backend/wwb.game/src/main/java/co/com/wwb/game/WebConfigurer.java
+++ backend/wwb.game/src/main/java/co/com/wwb/game/WebConfigurer.java
@@ -3,17 +3,18 @@ package co.com.wwb.game;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfigurer extends WebMvcConfigurerAdapter {
+public class WebConfigurer implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/rest/wwbgame/api/**")
-				.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
-				.allowedOrigins("http://localhost:4200");
+		registry.addMapping("/*")
+				.allowedOrigins("*")
+				.allowedMethods("*")
+				.allowedHeaders("*")
+				.allowCredentials(true);
 	}
-
 }
