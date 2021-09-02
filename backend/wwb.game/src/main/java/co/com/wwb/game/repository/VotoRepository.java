8f11da004349dc677ffdd422699e67f285b3b627
Carlos Zuluaga <carlos.zuluaga@gmail.com>
2021-09-01 21:37:58 -0500

    actualizacion del metodo save voto

--- backend/wwb.game/src/main/java/co/com/wwb/game/repository/VotoRepository.java
+++ backend/wwb.game/src/main/java/co/com/wwb/game/repository/VotoRepository.java
@@ -4,6 +4,15 @@ import org.springframework.data.repository.CrudRepository;
 
 import co.com.wwb.game.db.Voto;
 
+import java.util.List;
+
 public interface VotoRepository extends CrudRepository<Voto, String>{
+
+    /**
+     * Busca los votos efectuados por usuario
+     * @param usuario
+     * @return
+     */
+    List<Voto> findVotosByUsuario(final String usuario);
 
 }
